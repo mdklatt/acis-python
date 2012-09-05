12be494fca7a3d20f3c96f62ac1eb78296116caa
Michael Klatt <mdklatt@ou.edu>
2012-09-05 07:46:53 -0500

    util.py cleaned up and added to __init__.py

--- acis/__init__.py
+++ acis/__init__.py
@@ -6,3 +6,4 @@ __version__ = "0.1.dev"
 from error import *
 from result import *
 from request import *
+from util import *
--- acis/util.py
+++ acis/util.py
@@ -1,31 +1,45 @@
-"""
-ACIS utility functions.
+""" ACIS utility functions.
 
 """
-__version__ = '0.1.dev'
-__all__ = ('parse_sid',)
+__version__ = "0.1.dev"
 
 import re
 
-sid_types = {
-    1: 'WBAN',
-    2: 'COOP',
-    3: 'FAA',
-    4: 'WMO',
-    5: 'ICAO',
-    6: 'GHCN',
-    7: 'NWSLI',
-    8: 'RCC',
-    9: 'ThreadEx',
-    10: 'CoCoRaHS',
+__all__ = ("decode_sids",)
+
+
+_SID_REGEX = re.compile(r"(^[^ ]*) (\d+)$")
+
+_SID_TYPES = {
+     1: "WBAN",
+     2: "COOP",
+     3: "FAA",
+     4: "WMO",
+     5: "ICAO",
+     6: "GHCN",
+     7: "NWSLI",
+     8: "RCC",
+     9: "ThreadEx",
+    10: "CoCoRaHS",
 }
-sid_regex = re.compile(r'(^[^ ]*) (\d+)$')
-def parse_sid(sid):
-    try:
-        id, code = sid_regex.search(sid).groups()
-    except AttributeError:  # search returned None
-        raise ValueError('not a valid sid')
-    try:
-        return id, sid_types[int(code)]
-    except KeyError:
-        raise ValueError('unknown sid type')
+
+
+def decode_sids(sids):
+    """ Return a dict of site IDs keyed by their decoded ID type.
+
+    The 'sids' parameter is a list of SIDs from ACIS metadata where each SID is
+    a single string containing an identifier and its integer type code
+    separated by a space.
+
+    """
+    decoded = {}
+    for sid in sids:
+        try:
+            ident, code = _SID_REGEX.search(sid).groups()
+        except AttributeError:  # search returned None
+            raise ValueError("not a valid sid: %s" % sid)
+        try:
+            decoded[_SID_TYPES[int(code)]] = ident
+        except KeyError:
+            raise ValueError("unknown sid type: %s" % code)
+    return decoded
