a2935b3f18695cf0e4a182d00e5de26ad18d256c
Michael Klatt <mdklatt@ou.edu>
2012-09-28 14:26:26 -0500

    v0.1.2

--- acis/__version__.py
+++ acis/__version__.py
@@ -1,1 +1,7 @@
-__version__ = "0.1.x"
+""" The current version of the acis library package.
+
+This package uses the Semantic Versioning scheme:
+    <http://semver.org/>
+
+"""
+__version__ = "0.1.2"
