d5a036525bfa9ffc496173c0452b8ce8f562a68d
Michael Klatt <mdklatt@ou.edu>
2013-05-10 01:00:22 -0500

    added test command to setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,68 @@
-"""
-Distutils setup script for the acis library.
+""" Distutils setup script for the acis library.
 
-For basic installation in the current user's site-packages directory:
-    python setup.py install --user
+Basic command to run the test suite and install library in the user's package
+directory if all tests pass:
+
+    python setup.py test install --user
 
 """
-import distutils.core
-import sys
+from distutils.core import Command
+from distutils.core import setup
+from sys import exit
 
 from acis import __version__
+from test import run as run_tests
+
+SETUP_CONFIG = {
+    "name":         "acis",
+    "packages":     ("acis",),
+    "requires":     ("dateutil",),
+    "author":       "Michael Klatt",
+    "author_email": "mdklatt@ou.edu",
+    "url":          "https://github.com/mdklatt/acis-python",
+    "version":      __version__}
+
+
+class TestCommand(Command):
+    """ Custom setup command to run the test suite.
+    
+    """
+    description = "run the test suite"
+    user_options = []
+    
+    def initialize_options(self):
+        """ Set the default values for all options this command supports.
+        
+        """
+        return
+        
+    def finalize_options(self):
+        """ Set final values for all options this command supports.
+        
+        This is run after all other option assigments have been completed (e.g.
+        command-line options, other commands, etc.)
+        
+        """
+        return
+
+    def run(self):
+        """ Execute the command.
+        
+        """
+        if run_tests() != 0:
+            raise RuntimeError("test suite failed")
+        return
+        
 
 def main():
-    dist = {
-        "name":         "acis",
-        "version":      __version__,
-        "packages":     ("acis",),
-        "requires":     ("dateutil",),
-        "author":       "Michael Klatt",
-        "author_email": "mdklatt@ou.edu",
-        "url":          "https://github.com/mdklatt/acis-python",
-    }
-    distutils.core.setup(**dist)
+    """ Execute the setup commands.
+    
+    """
+    setup(cmdclass={"test": TestCommand}, **SETUP_CONFIG)
     return 0
 
 
+# Make the script executable.
+
 if __name__ == "__main__":
-    sys.exit(main())
+    exit(main())
